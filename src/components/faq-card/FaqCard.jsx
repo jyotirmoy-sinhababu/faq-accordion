bc40a7c97f973ad8d4dac2cb981fbd506838655a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-12-01 12:07:28 +0530

    faq card created

---
+++ src/components/faq-card/FaqCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import star from '../../assets/iconStar.svg';
+
+const FaqCard = () => {
+  return (
+    <div>
+      <div>
+        <img src={star} alt='star icon' />
+        <p>FAQs</p>
+      </div>
+    </div>
+  );
+};
+
+export default FaqCard;
--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './landingPageStyle.css';
+import FaqCard from '../../components/faq-card/FaqCard';
 
 const LandingPage = () => {
   return (
     <div className='outer-div'>
-      <div className='landing-cnt'></div>
+      <div className='landing-cnt'>
+        <FaqCard />
+      </div>
     </div>
   );
 };
