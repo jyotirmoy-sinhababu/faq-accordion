aa87c0c381a260d4ae6b52f3cc54f34ed2facff4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-12-02 17:59:55 +0530

    question and ans card created

---
+++ src/components/q&aCard/QandACard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const QandACard = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <p className='questions'> </p>
+        </div>
+        <div>
+          <p className='answers'></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QandACard;
